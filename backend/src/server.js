2f4786e40f8767c01b9109aa56c77e4e6d0afe8b
rahul810050 <rahul810050@gmail.com>
2025-08-13 17:27:11 +0800

    CreateChannelModal added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,14 @@ import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
 import chatRoutes from "./routes/chat.route.js";
 
+import cors from "cors";
+
 import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(clerkMiddleware()); // req.auth will be available in the request object
 
 app.get("/debug-sentry", (req, res) => {
