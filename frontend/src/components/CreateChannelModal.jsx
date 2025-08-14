956c8d520fcb6a5fd750d0a827a3f2996ef00d75
rahul810050 <rahul810050@gmail.com>
2025-08-14 14:10:26 +0800

    sidebar completed

--- frontend/src/components/CreateChannelModal.jsx
+++ frontend/src/components/CreateChannelModal.jsx
@@ -117,7 +117,7 @@ const CreateChannelModal = ({ onClose }) => {
         channelData.visibility = "private";
       } else {
         channelData.visibility = "public";
-        channelData.discoverable = true; // custom field that we add, we will use it later...
+        channelData.discoverable = true;
       }
 
       const channel = client.channel("messaging", channelId, channelData);
