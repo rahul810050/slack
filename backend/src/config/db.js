d258aab708a308e90bd0ceb9997bf8a4b255a62f
rahul810050 <rahul9875458880@gmail.com>
2026-08-08 04:34:03 +0530

    fixed db connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,30 @@
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
+let connectPromise = null;
+
+mongoose.set("bufferCommands", false);
+
 export const connectDB = async () => {
   if (!ENV.MONGO_URI) {
     throw new Error("MONGO_URI is not configured");
   }
 
-  const conn = await mongoose.connect(ENV.MONGO_URI);
+  if (mongoose.connection.readyState === 1) {
+    return mongoose;
+  }
+
+  if (connectPromise) {
+    return connectPromise;
+  }
+
+  connectPromise = mongoose.connect(ENV.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 5000,
+  });
+  const conn = await connectPromise;
+  connectPromise = null;
 
   console.log("MongoDB connected successfully:", conn.connection.host);
+  return conn;
 };
